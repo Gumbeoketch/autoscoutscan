bd0bf23e4468ca2bde426fa9db155d1210a8d2a7
hawk3yE <48472370+Gumbeoketch@users.noreply.github.com>
2023-09-27 11:30:25 +0300

    Update autoscout.sh

--- autoscout.sh
+++ autoscout.sh
@@ -1,13 +1,35 @@
-cd /opt 
-git clone https://github.com/nccgroup/ScoutSuite.git
-cd ScoutSuite
-pip install virtualenv
-virtualenv -p python3 venv || python3 -m virtualenv venv
-source venv/bin/activate
-pip install -r requirements.txt
-python scout.py aws
+#!/bin/bash
 
+# Function to run AWS Scout Suite
+run_aws_scan() {
+  pip install scoutsuite
+  scoutsuite aws --report-dir ./aws_report --no-browser
+}
 
+# Function to run GCP Scout Suite
+run_gcp_scan() {
+  pip install scoutsuite
+  echo "$GCP_CREDENTIALS_JSON" > gcp-credentials.json
+  scoutsuite gcp --report-dir ./gcp_report --key-file gcp-credentials.json
+}
 
-exit
-      
+# Function to run Azure Scout Suite
+run_azure_scan() {
+  pip install scoutsuite
+  scoutsuite azure --report-dir ./azure_report --client-id "$AZURE_CLIENT_ID" --client-secret "$AZURE_CLIENT_SECRET" --subscription-id "$AZURE_SUBSCRIPTION_ID" --tenant-id "$AZURE_TENANT_ID"
+}
+
+# Main menu
+echo "Select a cloud service provider:"
+echo "1. AWS"
+echo "2. GCP"
+echo "3. Azure"
+
+read -p "Enter your choice (1/2/3): " choice
+
+case $choice in
+  1) run_aws_scan ;;
+  2) run_gcp_scan ;;
+  3) run_azure_scan ;;
+  *) echo "Invalid choice. Please select 1, 2, or 3." ;;
+esac
